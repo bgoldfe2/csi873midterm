04dea1c23a5facfe5c73cda3346b554758ba75bd
Bruce Goldfeder <bgoldfe2@masonlive.gmu.edu>
2017-10-24 20:58:29 -0400

    I am through training hopefully and starting testing

--- bar.py
+++ bar.py
@@ -87,35 +87,8 @@ def initialize(ins, hids, outs, its, eps, lrn, mom, rd):
     output = outs
 
 
-    
-    
-def train(patterns,ai,ah,ao,wi,wo,ci,co):
-    
-    global iterations
-    global lrn_rate
-    global rate_decay
-   
-    # N: learning rate
-    for i in range(iterations):
-        error = 0.0
-        #random.shuffle(patterns)
-        for p in patterns:
-            inputs = p[0]
-            targets = p[1]
-            ai,ah,ao,wi,wo,ci,co = feedForward(inputs,ai,ah,ao,wi,wo,ci,co)
-            errorNew,ai,ah,ao,wi,wo,ci,co = backPropagate(targets,ai,ah,ao,wi,wo,ci,co)
-            error += errorNew
-        with open('error.txt', 'a') as errorfile:
-            errorfile.write(str(error) + '\n')
-            errorfile.close()
-        if i % 10 == 0:
-            print('error %-.5f' % error)
-        # learning rate decay
-        lrn_rate = lrn_rate * (lrn_rate / (lrn_rate + (lrn_rate * rate_decay)))
-        
-    return ai,ah,ao,wi,wo,ci,co
  
-def feedForward(inputs,ai,ah,ao,wi,wo,ci,co):
+def feedForward(cnt,inputs,ai,ah,ao,wi,wo,ci,co):
     
     #print (input,len(inputs))
     """
@@ -127,85 +100,111 @@ def feedForward(inputs,ai,ah,ao,wi,wo,ci,co):
         net = Sigma[i=0,n]wi*xi
         sigma(net) = 1/(1 + e**-net)
     """
-    inNum,cols = inputs.shape
-    print('num rows ',inNum)
     
     # input activations
-    for i in range(inNum):
-        ai[i] = inputs[i,1:] # remember to account for actual value in 0th index
+    # Get the training answer and replace it with 1 as the bias
 
+    ai[1:] = inputs[cnt,1:] # remember to account for actual value in 0th index
+    answer = inputs[cnt,0]
+    ai[0] = 1  # set the bias/threshold to always be 1
+    print('answer is',answer)
     # hidden activations
-    for j in range(hidden):
+    
+    #ah = np.multiply(ai,wi).sum(axis=0)
+    
+    for j in range(hidden+1):
         sum = 0.0
         for i in range(input):
             sum += ai[i] * wi[i][j]
         ah[j] = sigmoid(sum) #what about using tanh here?
 
     # output activations
+    ah[0] = 1 # set the bias/threshold to always be 1
     for k in range(output):
         sum = 0.0
-        for j in range(hidden):
+        for j in range(hidden+1):
             sum += ah[j] * wo[j][k]
         ao[k] = sigmoid(sum)
 
-    return ai,ah,ao,wi,wo,ci,co   
-    
-def backPropagate(targets,ai,ah,ao,wi,wo,ci,co):
-    
-    global output,hidden,momentum,lrn_rate
+    return answer,ai,ah,ao,wi,wo,ci,co  
+
+def calcDeltaKO(answer,ao):
     """
-    For the output layer
-    1. Calculates the difference between output value and target value
-    2. Get the derivative (slope) of the sigmoid function in order to determine how much the weights need to change
-    3. update the weights for every node based on the learning rate and sig derivative
-    For the hidden layer
-    1. calculate the sum of the strength of each output link multiplied by how much the target node has to change
-    2. get derivative to determine how much weights need to change
-    3. change the weights based on learning rate and derivative
-    :param targets: y values
-    :param N: learning rate
-    :return: updated weights
+    Calculates the delta_k for each output unit using the formula from the text
+    delta_k = ouput_k * (1-ouput_k)*(target_k - output_k)
     """
-    if len(targets) != output:
-        raise ValueError('Wrong number of targets you silly goose!')
+    
+    tk = int(answer)
+    #print('tk is ', tk)
+    tkArray = np.zeros(10)
+    tkArray[tk]=1
+    deltaKO = np.multiply(ao,(np.multiply((1 - ao),(tkArray - ao))))
+    #print('deltaKO is: ',deltaKO,' shape is ',deltaKO.shape)
+    
+    
+    return deltaKO
 
-    # calculate error terms for output
-    # the delta tell you which direction to change the weights
-    output_deltas = [0.0] * output
-    for k in range(output):
-        error = -(targets[k] - ao[k])
-        output_deltas[k] = sigmoid_prime(ao[k]) * error
+def calcDeltaKH(o_h,w_kh,d_k):
+    """
+    Calculates the delta_h for each hidden unit using the forumula from the text
+    delta_h = output_h * (1-ouput_h) * Sum(weight_kh * delta_ko)
+    o_h is the output of hidden units or the array in main 'ao'
+    w_kh is the weight of hidden units or the array in main 'wi'
+    d_k is the delta_k returned from the function 'calcDeltaKO' for output nodes
+    """
+    rows,cols = w_kh.shape
+    #print ('num rows,cols of d_k ',rows, '-',cols,' shape is ',w_kh.shape)
+    delta_h1 = np.multiply(o_h,(1-o_h))
+    sum = 0.0
+    for y in range(cols):
+        for x in range(rows):
+            delta_h2 = w_kh[x,y] * d_k[y]
+            sum += delta_h2
+    delta_h = np.multiply(delta_h1,sum)
+    
+    #print('deltaKH is: ',delta_h,' shape is ',delta_h.shape)    
+    return delta_h
 
-    # calculate error terms for hidden
-    # delta tells you which direction to change the weights
-    hidden_deltas = [0.0] * hidden
-    for j in range(hidden):
-        error = 0.0
-        for k in range(output):
-            error += output_deltas[k] * wo[j][k]
-        hidden_deltas[j] = tanh_prime(ah[j]) * error
-
+def updateWeights(answer,d_ko,d_kh,ai,ah,ao,wi,wo,ci,co,lrn_rate,hidden,output,momentum):
+    """
+    The weights are updated using the forumula from the text
+    w_ji <-- w_ji + Delta(w_ji)
+    
+    where
+    Delta(w_ji) = n*delta_j*x_ji
+    """
+    
+    # create tk the target/answer array of what output should be
+    tk = int(answer)
+    print('tk is ', tk)
+    tkArray = np.zeros(10)
+    tkArray[tk]=1
+    
     # update the weights connecting hidden to output
-    for j in range(hidden):
+    # the co array represents the n-1 or prior iteration delta value
+    for j in range(hidden+1):
         for k in range(output):
-            change = output_deltas[k] * ah[j]
-            wo[j][k] -= lrn_rate * change + co[j][k] * momentum
-            co[j][k] = change
+            delta = lrn_rate * d_ko[k] * ah[j] + (momentum * co[j][k])
+            #print('delta is ',delta)
+            wo[j][k] -= wo[j][k] + delta
+            co[j][k] = delta
 
     # update the weights connecting input to hidden
-    for i in range(input):
-        for j in range(hidden):
-            change = hidden_deltas[j] * ai[i]
-            wi[i][j] -= lrn_rate * change + ci[i][j] * momentum
-            ci[i][j] = change
+    for i in range(input+1):      # add in w0 threshold term
+        for j in range(hidden+1): # add in w0 threshold term
+            delta = lrn_rate * d_kh[j] * ai[i] + momentum * ci[i][j]
+            wi[i][j] -= wi[i][j] + delta
+            ci[i][j] = delta
+         
 
     # calculate error
     error = 0.0
-    for k in range(len(targets)):
-        error += 0.5 * (targets[k] - ao[k]) ** 2
+    for k in range(output):
+        #print('tk is ',tkArray[k],' ouput is ',ao[k])
+        error += (tkArray[k] - ao[k]) ** 2 
+        
+    print('the summed square error is ',error)
     
-    return error,ai,ah,ao,wi,wo,ci,co
-
 def ReadInFiles(path,trnORtst):
     # This reads in all the files from a directory filtering on what the file
     # starts with
@@ -217,7 +216,7 @@ def ReadInFiles(path,trnORtst):
             data = np.loadtxt(path + "\\" + fname)
             fullData.append(data)
     numFiles = len (fullData)
-    print(numFiles)
+    #print(numFiles)
    
     return fullData
 
@@ -282,11 +281,13 @@ def main():
     #np.savetxt('fooout.txt',my_data,fmt='%1i')
     
     # Convert the 0-255 to 0 or 1 values in data
-    my_data /= 255.0
+    my_data[:,1:] /= 255.0
+    print('first column is ',my_data[:,1:])
     
-    print(my_data[0,:])
+    inNum,cols = my_data.shape
+    print('num rows ',inNum)
     
-    initialize(784, 100, 10, its=50, eps = 3,lrn = 0.5, mom = 0.5, rd = 0.01)
+    initialize(784, 4, 10, its=50, eps = 3,lrn = 0.5, mom = 0.5, rd = 0.01)
 
     print_params()
     
@@ -295,51 +296,52 @@ def main():
     ah = np.ones(hidden+1) # plus one for the bias/threshold unit
     ao = np.ones(output)
     
-    print ('input array inited',ai)
+    #print ('input array inited',ai)
 
 
     # create random weights between -0.05 and 0.05 as per text on pg. 98
     # plus one for the bias/threshold unit
-    wi = np.random.uniform(-0.05, 0.05, size = (input+1, hidden))
+    wi = np.random.uniform(-0.05, 0.05, size = (input+1, hidden+1))
     wo = np.random.normal(-0.05, 0.05, size = (hidden+1, output))
     
-    print('wi array random inited',wi)
+    #print('wi array random inited',wi)
     
     # temporary arrays to hold the numbers to be updated each iteration
-    ci = np.zeros((input+1, hidden))
+    ci = np.zeros((input+1, hidden+1))
     co = np.zeros((hidden+1, output))
     
-    feedForward(my_data,ai,ah,ao,wi,wo,ci,co)
-    
-    
-    
-    asdf
-    
-    
-    # Read in the Training data first
-    dataset = ReadInFiles(dpath,'train')
-    my_data = ReadInOneList(dataset,trnNum)
-    #np.savetxt('fooout.txt',my_data,fmt='%1i')
+    # Iterate over the range of total images for training
+    for imgNum in range(inNum):
+        answer,ai,ah,ao,wi,wo,ci,co = feedForward(imgNum,my_data,ai,ah,ao,wi,wo,ci,co)
+        
+        print ('answer is ',answer)
+        
+        # Calculate the error term deltaKO for each output unit
+        deltaKO = calcDeltaKO(answer,ao)
+        
+        # Calculate the error term deltaKH for each hidden unit
+        deltaKH = calcDeltaKH(ah,wo,deltaKO)
+        
+        #print ('delta h is ',deltaKH)
+        
+        updateWeights(answer,deltaKO,deltaKH,ai,ah,ao,wi,wo,ci,co,lrn_rate,hidden,output,momentum)
     
     # Read in the test data
     dataset2 = ReadInFiles(dpath,'test')
-    my_test = ReadInOneList(dataset2,tstNum)
+    my_test = ReadInOneList(dataset2,tstNum) 
     
-###### Neural Net Structure and Behavior Variables ######
+    # Convert the 0-255 to 0 or 1 values in data
+    my_test[:,1:] /= 255.0
+    print('first column is ',my_test[:,1:])
     
-    # array of the layers - in the midterm this is three layers
-    # The layers go in order from left to right - [input,hidden,output]
-    layers = np.array(())
+    tstNum,cols = my_test.shape
+    print('num rows ',tstNum)
     
+    for imgNum in range(2):
     
-    outputs = Output(results,tstNum)
-    print(outputs)
-    print(outputs.sum(axis=0))
-    
-    # print the percentages per number tested
-    print(outputs/tstNum)
-    
-    # print the percentage correct over all the numbers tested
-    print (outputs.sum(axis=0)/(tstNum*10))
+        answer,ai,ah,ao,wi,wo,ci,co = feedForward(imgNum,my_test,ai,ah,ao,wi,wo,ci,co)
+        testAnswer = ao.argmax(axis=0)
+        print('Test Answer is ',testAnswer)
+
     
 main()
